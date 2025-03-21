79f69bc27d4969b67c505eee6215a4016ec6d805
littleflockkmb <littleflockprayerfellowship.web@gmail.com>
2025-03-22 00:49:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json()); // Middleware to parse JSON requests
 
 // Connect to MongoDB
-mongoose.connect('mongodb+srv://littleflockprayerfellowshipweb:@littleflock-123@littleflockweb.7aaya.mongodb.net/?retryWrites=true&w=majority&appName=littleflockweb')
+mongoose.connect('mongodb+srv://littleflockprayerfellowshipweb:littleflock-123@littleflockweb.7aaya.mongodb.net/?retryWrites=true&w=majority&appName=littleflockweb')
   .then(() => {
     console.log('Connected to MongoDB!');
   })
