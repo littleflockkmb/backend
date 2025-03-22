6644938e428447495c5cfab5d5b0f039f957ab55
littleflockkmb <littleflockprayerfellowship.web@gmail.com>
2025-03-22 17:26:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(cors({ origin: 'https://vbs-pink.vercel.app' })); // Allow requests from
 app.use(express.json()); // Parse JSON requests
 
 // Connect to MongoDB
-mongoose.connect('your-mongodb-connection-string', {
+mongoose.connect('mongodb+srv://littleflockprayerfellowshipweb:flock123@littleflockweb.7aaya.mongodb.net/?retryWrites=true&w=majority&appName=littleflockweb', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
