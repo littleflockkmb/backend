68e6c97463586a9c3286f34814025d62b4269258
littleflockkmb <littleflockprayerfellowship.web@gmail.com>
2025-03-26 13:05:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,8 +10,7 @@ app.use(express.json()); // Parse JSON requests
 
 // Connect to MongoDB
 mongoose.connect('mongodb+srv://littleflockprayerfellowshipweb:flock123@littleflockweb.7aaya.mongodb.net/?retryWrites=true&w=majority&appName=littleflockweb', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+  useUnifiedTopology: true
 })
   .then(() => console.log('Connected to MongoDB!'))
   .catch((error) => console.error('Error connecting to MongoDB:', error));
